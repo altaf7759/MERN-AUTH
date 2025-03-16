2afcb81978d1e76f7e6cd4d5fbe1cb09bac97b2c
Altaf Raja <altafraja7369892024.com>
2025-03-16 13:45:25 +0530

    cookie

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -85,7 +85,7 @@ export const login = async (req, res) => {
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({ success: true });
+    res.status(200).json({ success: true, message: "Login successful!" });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
